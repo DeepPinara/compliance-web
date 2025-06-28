6e3870dba2754eb7ae2e5069cdc3992b9ceea1cb
Himnandini Sharma <sharmahimnandini@gmail.com>
2025-06-28 13:45:33 +0530

    React router dom has been installed and layouts files are created

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Header from './Header'
+
+function Layout() {
+    return (
+        <div>
+            Sidebar
+        </div>
+    )
+}
+
+export default Layout
